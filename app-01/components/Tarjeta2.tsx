a81125a1b88cb660a1f2d4ba5a0eaa07ff5dd9b1
augusto5959 <augustoviteri98@gmail.com>
2024-12-27 21:48:27 -0500

    asd

--- app-01/components/Tarjeta2.tsx
+++ app-01/components/Tarjeta2.tsx
@@ -17,14 +17,16 @@ export default function Tarjeta2(props: any) {
 			<Text style={styles.txtNombre}>
 				{props.datos.name.first} {props.datos.name.last}
 			</Text>
-			<Modal visible={visible}>
+			<Modal visible={visible} transparent={true}>
 				<View style={styles.modal}>
 					<Text style={styles.txtNombre}>
 						{props.datos.name.first} {props.datos.name.last}
 					</Text>
 					<View style={styles.fila}>
 						<Image style={styles.img} source={{ uri: props.datos.images.main }} />
-						<Text>{props.datos.occupation}</Text>
+						<View style={styles.ocupacion}>
+							<Text style={{fontSize:20}}>{props.datos.occupation}</Text>
+						</View>
 					</View>
 
 					<Button title='Cerrar' onPress={() => setVisible(false)} />
@@ -37,25 +39,33 @@ export default function Tarjeta2(props: any) {
 const styles = StyleSheet.create({
 	txtNombre: {
 		fontSize: 40,
-		justifyContent:'center',
-		textAlign:'center'
+		justifyContent: 'center',
+		textAlign: 'center',
+		fontWeight: 'bold',
 	},
 	container: {
 		backgroundColor: '#ecc1ac',
 		margin: 2,
 		borderRadius: 20,
-		gap:10,
+		gap: 10,
 	},
 	modal: {
 		backgroundColor: '#adadad',
 		flex: 1,
+		justifyContent: 'center',
+		gap: 70,
+		padding: 40,
 	},
 	img: {
 		width: 200,
 		height: 200,
 		resizeMode: 'contain',
 	},
-	fila:{
-		flexDirection:'row'
-	}
+	fila: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+	},
+	ocupacion: {
+		justifyContent: 'center',
+	},
 });
